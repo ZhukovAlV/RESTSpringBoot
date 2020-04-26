4dfa0463b771f1ffef6c950efc12b30b0d7063ea
plinio <avzhykov@gmail.com>
2020-04-26 22:05:44 +0600

    Create project

---
+++ src/main/java/com/example/RESTfulSpringBoot/ResTfulSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.RESTfulSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RESTfulSpringBoot/ResTfulSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTfulSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
